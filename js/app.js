12a87772e148876f316b1af5964dfd50c3a9534a
qazarr <d.makarow@wp.pl>
2014-11-30 13:27:03 +0100

    add slick

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
 $(document).foundation();
+(function ($) {
+    var App = {
+        init: function () {
+            this.slickStart();
+        },
+        slickStart: function () {
+            $('.brands').slick({
+                slidesToShow: 8,
+                slidesToScroll: 8
+            });
+
+            var filtered = false;
+            $('.filter-sport').on('click', function () {
+                if (filtered === false) {
+                    $('.brands').slickFilter('.sport');                    
+                    filtered = true;
+                } else {
+                    $('.sport').slickUnfilter();                    
+                    filtered = false;
+                }
+            });
+            $('.filter-uslugi').on('click', function () {
+                if (filtered === false) {
+                    $('.brands').slickFilter('.uslugi');                    
+                    filtered = true;
+                } else {
+                    $('.uslugi').slickUnfilter();                    
+                    filtered = false;
+                }
+            });
+            $('.filter-suplementy').on('click', function () {
+                if (filtered === false) {
+                    $('.brands').slickFilter('.suplementy');                    
+                    filtered = true;
+                } else {
+                    $('.suplementy').slickUnfilter();                    
+                    filtered = false;
+                }
+            });
+
+        }
+    }
+    App.init();
+    
+})(jQuery);
